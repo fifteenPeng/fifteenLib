8a68dd764fe0f0f50119901db5db85d066a677f4
PengFei <fifteenpeng@163.com>
2020-12-18 19:05:00 +0800

    完成数值提取，数值输出

--- CStringLib/inc/fifteenString.h
+++ CStringLib/inc/fifteenString.h
@@ -4,18 +4,62 @@
 作者：fifteenpeng
 描述：本库用于日常处理字符文件，提取重要数据。处理ASCII命令等信息
 ******************************************************************** */
-
-
 #include "../inc/DataType.h"
 
+/* 测试函数开启编译 */
+#define FIFTEEN_TEST
+
+typedef struct GSNStr
+{
+     int32_t NumP;
+    // int64_t NumP;
+    uint16_t ProcLen;
+    uint16_t DataLen;
+    uint32_t Magn;
+    /* 是否带符号输出 */
+    uint8_t symbol;
+    /* data */
+}GSNStr_t;
 
 /* *******************************
 函数名：
-描述：输入字符串，提取出数值返回值均为整数
+描述：输入字符串，提取出数值返回值均为整数，皆为带符号处理
+    如果是小数，则放大为整数接收
+
 输入：
+    * buff    需要处理的字符串指针
+    * GSNStr_t 相关数据信息结构体
+    DataLen 指定处理数据长度，为0时则直接处理
 输出：
 返回：-1 提取失败  1提取成功
+
+错误描述：
+1.指定数据长度时，遇到非数字数据
+2.处理数据超过int32_t数值
+3.
 ********************************** */
-int8_t GetStringNumber(uint8_t * cp,void * NumP,uint8_t symb,uint8_t NumTpye);
+int8_t FB_GetStringNumber(uint8_t * Buff,GSNStr_t *Ptr,uint16_t BuffLen);
+/* *******************************
+函数名：
+描述：设置返回的字符串长度
+
+输入：
+    * buff    输出的数组
+    * GSNStr_t 相关数据信息结构体
+    IntLen  整数的所占的位数
+输出：
+返回：-1 转换失败  1转换成功
+
+错误描述：
+1.数据超出指定长度
+********************************** */
+// int8_t GetStringNumber(uint8_t * Buff,int32_t * NumP,uint16_t * ProcLen ,uint32_t * Magn,uint16_t DataLen)
+int8_t FB_SetStringNumber(uint8_t * Buff,GSNStr_t *Ptr,uint16_t IntLen);
+
+
+#ifdef FIFTEEN_TEST
+int8_t FB_GetStringNumber_test(uint8_t flag);
+int8_t FB_SetStringNumber_test(uint8_t flag);
+#endif
 
 #endif
